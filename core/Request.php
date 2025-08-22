d0a656da050e9cb9c978edb42932c7f8cffbbfcd
MigsGit <miguellgsp12345@gmail.com>
2025-08-22 14:11:44 +0800

    Add Application, Router & Request

---
+++ core/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+class Request {
+    
+    public function getPath() {
+        $path     = $_SERVER['REQUEST_URI'] ?? '/';
+        echo $position = strpos( $path, '?' );
+    }
+
+    public function getMethod() {
+
+    }
+}   
--- public/index.php
+++ public/index.php
@@ -10,7 +10,21 @@ define('APP_ROOT', dirname(__DIR__));
  * Register The Auto Loader.
  */
 
+if (!file_exists(APP_ROOT . '/vendor/autoload.php')){
+    echo '/vendor/autoload.php not exists';
+}
 require_once APP_ROOT . '/vendor/autoload.php';
 
 
-echo '/vendor/autoload.php is exists.';
+use App\Application;
+
+$app = new Application();
+
+
+$app->router->get( '/hello', function () {
+    echo 'hello';
+    return;
+} );
+
+$app->run();
+echo json_encode($app);
